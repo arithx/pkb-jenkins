dfbee1219409c341ce849973e62a74186877887e
Stephen Lowrie <stephen.lowrie@rackspace.com>
2016-06-15 11:36:20 -0500

    Change from source to .

--- pkb-aio.sh
+++ pkb-aio.sh
@@ -5,6 +5,6 @@ pip install -r requirements.txt
 pip install -r requirements-openstack.txt
 pip install .
 
-source /opt/stack/devstack/openrc
+. /opt/stack/devstack/openrc
 
 python pkb.py --cloud=OpenStack --benchmarks=cluster_boot --image=cirros-0.3.4-x86_64-uec --config_override=cluster_boot.vm_groups.default.vm_count=1 --openstack_image_username cirros
